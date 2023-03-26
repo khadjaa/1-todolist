58a1d56f4fba0fbafda75d387d391e28dcf00541
khadjaa <islomkhadja2001@icloud.com>
2023-03-26 20:54:34 +0300

    added Button from MU

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -1,4 +1,6 @@
 import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import Button from "@mui/material/Button";
+import {styled} from "@mui/material";
 
 export type AddItemFormPropsType = {
     addItem: (newTitle: string) => void
@@ -28,12 +30,31 @@ const AddItemForm = (props: AddItemFormPropsType) => {
         }
     }
 
+    const ButtonStyled = styled(Button)({
+        backgroundColor: '#0063cc',
+        maxWidth: '30px',
+        maxHeight: '30px',
+        minWidth: '30px',
+        minHeight: '30px',
+
+        '&:hover': {
+            backgroundColor: '#0069d9',
+            borderColor: '#0062cc',
+            boxShadow: 'none',
+        }
+    })
+
+
+
     return (
         <div>
             <input value={title}
                    onChange={onChangeTitleHandler}
                    onKeyDown={onKeyDownAddTaskHandler}/>
-            <button onClick={onClickAddTaskHandler}>+</button>
+            {/*<button onClick={onClickAddTaskHandler}>+</button>*/}
+            <ButtonStyled variant={'contained'}
+                    onClick={onClickAddTaskHandler}
+            >+</ButtonStyled>
         </div>
     );
 };
