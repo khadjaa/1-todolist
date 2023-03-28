837c74cb5b4a8e905c9e2795a017b53a6e0b1ba9
khadjaa <islomkhadja2001@icloud.com>
2023-03-28 15:20:45 +0300

    added add case in reducer

--- src/store/todolist-reducer.ts
+++ src/store/todolist-reducer.ts
@@ -1,4 +1,5 @@
 import {TodolistType} from "../App";
+import {v1} from "uuid";
 
 type ActionType = {
     type: string
@@ -9,6 +10,9 @@ export const TodoListsReducer = (state: TodolistType[], action: ActionType) => {
     switch (action.type) {
         case 'REMOVE-TODOLIST':
             return state.filter(el => el.id !== action.id)
+        case 'ADD-TODOLIST':
+            const newTodolist =  {id: v1(), title: action.title, filter: 'all'}
+            return [newTodolist, ...state]
         default:
             throw new Error('I don\'t understand this type')
     }
