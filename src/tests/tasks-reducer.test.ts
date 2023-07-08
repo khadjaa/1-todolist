aec3246a5421ac5366042337fc641df365cf41c0
khadjaa <islomkhadja2001@icloud.com>
2023-07-08 14:16:43 +0300

    fixed bug with jest

--- src/tests/tasks-reducer.test.ts
+++ src/tests/tasks-reducer.test.ts
@@ -1,5 +1,5 @@
 import {
-    addTaskAC,
+    addTaskAC, removeTaskAC,
     tasksReducer,
     TasksStateType
 } from '../features/Todolists/tasks-reducer'
@@ -64,23 +64,13 @@ beforeEach(() => {
     }
 })
 
-// test('correct task should be deleted from correct array', () => {
-//     const action = removeTaskAC('2', 'todolistId2')
-//
-//     const endState = tasksReducer(startState, action)
-//
-//     expect(endState).toEqual({
-//         'todolistId1': [
-//             {id: '1', title: 'CSS', isDone: false},
-//             {id: '2', title: 'JS', isDone: true},
-//             {id: '3', title: 'React', isDone: false}
-//         ],
-//         'todolistId2': [
-//             {id: '1', title: 'bread', isDone: false},
-//             {id: '3', title: 'tea', isDone: false}
-//         ]
-//     })
-// })
+test('correct task should be deleted from correct array', () => {
+    const action = removeTaskAC('1', 'todolistId1')
+
+    const endState = tasksReducer(startState, action)
+
+    expect(endState['todolistId1'].length).toBe(1)
+})
 
 test('correct task should be added to correct array', () => {
     const task = {
